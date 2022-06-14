e3f009866d9eb3cc24c812ad84205ac83e148f5b
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2022-06-14 21:24:15 +0200

    Added GitHub Link

--- src/Header.js
+++ src/Header.js
@@ -7,14 +7,19 @@ export default function Header() {
         <div className={StyleSheet.Header}>
             <div className={StyleSheet.inner}>
                 <Link to="/">League Challenges</Link>
+                <Link to="/faq">FAQ</Link>
             </div>
         </div>
+
+        <a className={StyleSheet.git} href="https://github.com/DarkIntaqt/challenges" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i> open source</a>
+
         <div className={StyleSheet.HeaderPlaceholder}>
 
         </div>
         <div className={StyleSheet.content}>
+
             <Outlet></Outlet>
         </div>
 
-    </React.Fragment>
+    </React.Fragment >
 }
