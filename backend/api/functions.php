e463d5b33476996eb97ab4f67566ddd01e4d2af1
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2022-06-25 18:34:48 +0200

    Cleaner code: removed unexecutable code

--- backend/api/functions.php
+++ backend/api/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-    $titles = json_decode(file_get_contents($_SERVER["HOME"]."/cdn/lol/static/titles.json"),true);
+    $titles = json_decode(file_get_contents("/var/www/cdn/lol/static/titles.json"),true);
 
     function curlGet($url) {
         $data = curl_init($url);
@@ -31,80 +31,59 @@
           } else {
               $f .= $value;
           }
-        }
+        
+          }
         return $f;
       }
       
       function getNextLevel($r){
-        switch ($r) {
-          case 'NONE':
-            return "IRON";
-            break; 
-          case 'IRON':
-            return "BRONZE";
-            break;
-          case 'BRONZE':
-            return "SILVER";
-            break;
-          case 'SILVER':
-            return "GOLD";
-            break;
-          case 'GOLD':
-            return "PLATINUM";
-            break;
-          case 'PLATINUM':
-            return "DIAMOND";
-            break;
-          case 'DIAMOND':
-            return "MASTER";
-            break;
-          case 'MASTER':
-            return "GRANDMASTER";
-            break;
-          case 'GRANDMASTER':
-            return "CHALLENGER";
-            break;
-          default:
-            return "MAXED";
-            break;
-        }
-      }
+      switch ($r) {
+        case 'NONE':
+          return "IRON";
+        case 'IRON':
+          return "BRONZE";
+        case 'BRONZE':
+          return "SILVER";
+        case 'SILVER':
+          return "GOLD";
+        case 'GOLD':
+          return "PLATINUM";
+        case 'PLATINUM':
+          return "DIAMOND";
+        case 'DIAMOND':
+          return "MASTER";
+        case 'MASTER':
+          return "GRANDMASTER";
+        case 'GRANDMASTER':
+          return "CHALLENGER";
+        default:
+          return "MAXED";
+      }}
       
       function levelToNum($r) {
         switch ($r) {
           case 'NONE':
             return 0;
-            break; 
           case 'IRON':
             return 1;
-            break;
           case 'BRONZE':
             return 2;
-            break;
           case 'SILVER':
             return 3;
-            break;
           case 'GOLD':
             return 4;
-            break;
           case 'PLATINUM':
             return 5;
-            break;
           case 'DIAMOND':
             return 6;
-            break;
           case 'MASTER':
             return 7;
-            break;
           case 'GRANDMASTER':
             return 8;
-            break;
           case 'CHALLENGER':
             return 9;
-            break;
           default:
             return -1;
-            break;
         }
       }
 
@@ -112,70 +91,59 @@
         switch ($num) {
           case 0:
             return "NONE";
-            break;
           case 1:
             return "IRON";
-            break;
           case 2:
             return "BRONZE";
-            break;
           case 3:
             return "SILVER";
-            break;
           case 4:
             return "GOLD";
-            break;
           case 5:
             return "PLATINUM";
-            break;
           case 6:
             return "DIAMOND";
-            break;
           case 7:
             return "MASTER";
-            break;
           case 8:
             return "GRANDMASTER";
-            break;
           case 9:
             return "CHALLENGER";
-            break;
           default:
             return "UNDEFINED";
-            break;
         }
       }
 
 
-    $challenges = @json_decode(@file_get_contents($_SERVER["HOME"]."/cdn/lol/static/challenges-{$api->getRegion()["region"]}.json"),true);
+      $challenges = @json_decode(@file_get_contents("/var/www/cdn/lol/static/challenges-{$api->getRegion()["region"]}.json"),true);
 
     function time_elapsed_string($datetime, $full = false) {
-      $now = new DateTime;
-      $ago = new DateTime($datetime);
-      $diff = $now->diff($ago);
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
 
-      $diff->w = floor($diff->d / 7);
-      $diff->d -= $diff->w * 7;
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
 
-      $string = array(
-        'y' => 'year',
-        'm' => 'month',
-        'w' => 'week',
-        'd' => 'day',
-        'h' => 'hour',
-        'i' => 'minute',
-        's' => 'second',
-      );
-      foreach ($string as $k => &$v) {
-        if ($diff->$k) {
-          $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
-        } else {
-          unset($string[$k]);
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
         }
-      }
 
-      if (!$full) $string = array_slice($string, 0, 1);
-      return $string ? implode(', ', $string) . ' ago' : 'just now';
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
     }
 
 ?>
