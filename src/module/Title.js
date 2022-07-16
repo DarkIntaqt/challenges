a73855e78714b2176fe4c10ff1d43c85cf273a77
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2022-07-16 18:03:23 +0200

    Lazy load title images

--- src/module/Title.js
+++ src/module/Title.js
@@ -3,6 +3,7 @@ import css from "../css/titles.module.css"
 import Error from "./Error"
 import { Navigate } from "react-router-dom"
 import get from "../func/get"
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 export default class Title extends Component {
     constructor(props) {
@@ -56,8 +57,8 @@ export default class Title extends Component {
             const element = this.state.titles[i];
 
             titles.push(<a href={"/challenge/" + element.cid} className={css.title + " " + element.type + " clearfix"} key={element.cid} challengeid={element.cid} onClick={this.goTo}>
-                <img src={"https://lolcdn.darkintaqt.com/s/C-" + element.icon + "-" + element.type.toLowerCase()} alt="" />
-                <span>Achieved by {element.percentile}% of all players</span>
+                <LazyLoadImage height={45} width={45} src={"https://lolcdn.darkintaqt.com/s/C-" + element.icon + "-" + element.type.toLowerCase()} placeholderSrc={"https://cdn.darkintaqt.com/lol/static/missing/item.png"} alt="" />
+                <p>Achieved by {element.percentile}% of all players</p>
                 <h2>{element.title}</h2>
                 <p>Reach <span>{element.type} tier</span> in <span>"{element.challenge}"</span>. <br></br>{element.description}<br /><br /><i>Need {element.threshold}.</i></p>
             </a>)
