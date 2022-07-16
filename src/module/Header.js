fe933f4ce7710065f880ba39454109567cf14b53
Ole <61588850+DarkIntaqt@users.noreply.github.com>
2022-07-16 23:46:52 +0200

    remove "report bugs" option

--- src/module/Header.js
+++ src/module/Header.js
@@ -5,7 +5,7 @@ import logo from "../img/logo.png";
 
 export default function Header() {
 
-    const messages = ["provide feedback", "open source", "report bugs"];
+    const messages = ["provide feedback", "open source"];
     const message = messages[Math.floor(Math.random() * messages.length)]
 
     return <Fragment>
