1a2c1728b955063606d1a6ac0cdf9b9d5e91cde2
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2022-10-03 11:06:52 +0200

    rebrand header text

--- src/module/Header-n-Footer.js
+++ src/module/Header-n-Footer.js
@@ -6,22 +6,27 @@ import Ad from "./Ad"
 
 export default function HeaderNFooter() {
 
-    const messages = ["provide feedback", "open source", "request feature"];
-    const message = messages[Math.floor(Math.random() * messages.length)]
-
     return <Fragment>
         <nav className={StyleSheet.Header}>
             <div className={StyleSheet.inner}>
-                <Link to="/"><img src={logo} alt="League Challenges Logo" />League Challenge Tracker</Link>
-                <Link to="/challenges"><i class="fa-solid fa-list"></i> Challenges</Link>
-                <Link to="/challenge/0" className={StyleSheet.hideOnMobile}><i class="fa-solid fa-ranking-star"></i> Leaderboards</Link>
-                <Link to="/titles"><i class="fa-solid fa-award"></i> Titles</Link>
+                <Link to="/">
+                    <img src={logo} alt="League of Legends Challenge Tracker Logo" />
+                    Challenge Tracker
+                </Link>
+
+                <Link to="/challenges">
+                    <i className="fa-solid fa-list"></i> Challenges
+                </Link>
+
+                <Link to="/challenge/0" className={StyleSheet.hideOnMobile}>
+                    <i className="fa-solid fa-ranking-star"></i> Leaderboards
+                </Link>
+
+                <Link to="/titles">
+                    <i className="fa-solid fa-award"></i> Titles
+                </Link>
             </div>
         </nav>
-
-        <a className={StyleSheet.git} href="https://github.com/DarkIntaqt/challenges" target="_blank" rel="noreferrer">
-            <i className="fa-brands fa-github"></i> {message}
-        </a>
 
         <div className={StyleSheet.HeaderPlaceholder} id="top"></div>
 
@@ -36,16 +41,17 @@ export default function HeaderNFooter() {
                 <div className={StyleSheet.leftFooter}>
                     <Link to={"/"}>
                         <img src={logo} alt="Logo" width={50} height={50}></img>
-                        <p>League Challenges</p>
+                        <p>LoL Challenge Tracker</p>
                     </Link>
                 </div>
                 <div className={StyleSheet.centerFooter} data-nosnippet>
                     <p>'Challenges.DarkIntaqt.Com' isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.<br /><br />Huge thanks to <a href="https://www.communitydragon.org" target="_blank" rel="noreferrer">CommunityDragon</a> for hosting the assets. </p>
                 </div>
                 <div className={StyleSheet.rightFooter}>
-                    <a href="https://darkintaqt.com/assets/impressum/" rel="noreferrer" target="_blank">- Imprint</a>
-                    <a href="https://darkintaqt.com/assets/privacypolicy/" rel="noreferrer" target="_blank">- Privacy Policy</a>
-                    <Link to={"/faq"}>- FAQ</Link>
+                    <a href="https://darkintaqt.com/assets/impressum/" rel="noreferrer" target="_blank">Imprint</a>
+                    <a href="https://darkintaqt.com/assets/privacypolicy/" rel="noreferrer" target="_blank">Privacy Policy</a>
+                    <Link to={"/faq"}>FAQ</Link>
+                    <a href="https://github.com/DarkIntaqt/challenges" target="_blank" rel="noreferrer">GitHub</a>
                 </div>
             </div>
         </footer>
