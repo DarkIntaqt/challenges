fe11ee70b6a0f281e1852c67e66cc20f5c741466
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2022-10-16 13:18:17 +0200

    add a capitalize function

--- src/func/stringManipulation.js
+++ src/func/stringManipulation.js
@@ -3,3 +3,22 @@ export function strtolower(value) {
     return ("" + value).toLowerCase();
 }
 
+
+export function capitalize(input) {
+
+    if (typeof input === "string") {
+
+        if (input.length > 1) {
+
+            // set to lowercase first
+            input = strtolower(input)
+
+            input = input.split(" ").length > 0 ? input.split(" ").map((item) => item[0].toUpperCase() + item.substring(1)).join(" ") : input[0].toUpperCase() + input.substring(1)
+
+        }
+
+    }
+
+    return input
+
+}
