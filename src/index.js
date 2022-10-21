1c5e15bd1929fec3ea771bb0416547e625f65a8b
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2022-10-21 17:28:08 +0200

    fix replaceAll is not a function

--- src/index.js
+++ src/index.js
@@ -25,106 +25,101 @@ import Loadable from 'react-loadable';
 function main() {
 
   const root = createRoot(document.getElementById('root'));
-  try {
-    const windowVariables = config.windowVariables
-    Object.keys(windowVariables).forEach(function (key, index) {
-      if (!checkFor(window[key])) {
-        console.info(`'${key}' does not exist, set to default value. `)
-        window[key] = windowVariables[key]
-      }
-    });
 
-
-
-    if (window.gC) {
-      if (window.gC("_Cregion")) {
-        window.region = window.gC("_Cregion")
-      } else {
-        // Set region cookie if it is not already set
-        let date = new Date();
-        date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
-        const expires = date.toUTCString();
-
-        document.cookie = "_Cregion=" + window.region + ";expires=" + expires + ";path=/;Secure"
-      }
+  const windowVariables = config.windowVariables
+  Object.keys(windowVariables).forEach(function (key, index) {
+    if (!checkFor(window[key])) {
+      console.info(`'${key}' does not exist, set to default value. `)
+      window[key] = windowVariables[key]
     }
+  });
 
-    const ScrollToTop = ({ children }) => {
-      const location = useLocation();
-      useLayoutEffect(() => {
-        document.documentElement.scrollTo(0, 0);
-      }, [location.pathname]);
-      return children
+
+
+  if (window.gC) {
+    if (window.gC("_Cregion")) {
+      window.region = window.gC("_Cregion")
+    } else {
+      // Set region cookie if it is not already set
+      let date = new Date();
+      date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+      const expires = date.toUTCString();
+
+      document.cookie = "_Cregion=" + window.region + ";expires=" + expires + ";path=/;Secure"
     }
-
-    const Community = Loadable({
-      loader: (content) => import('./module/Community'),
-      loading: function () {
-        return <div style={{ width: "100%", float: "left" }}>
-          <Loader />
-          <p style={{ color: "white", fontSize: "1rem", textAlign: "center" }}>Loading...</p>
-        </div>
-      },
-    });
-
-    root.render(<BrowserRouter>
-
-      {/* SCROLL TO TOP ON ROUTE CHANGE */}
-      <ScrollToTop>
-
-        <Routes>
-
-          <Route path="/" element={<Fragment>
-            <HeaderNFooter />
-            <VariableProxy />
-          </Fragment>}>
-
-            <Route path="" element={<Start />}></Route>
-
-            <Route path="faq" element={<FAQ />}></Route>
-
-            <Route path="community" element={<Community />}></Route>
-
-            <Route path="titles" element={<Title />}></Route>
-
-            <Route path="challenges" element={<Challenges />}></Route>
-
-            <Route path="challenge/:id" element={<PathProxy />}></Route>
-
-            <Route path=":server/:user/*" element={<PathProxy />}></Route>
-
-            <Route path="*" element={<Error />}></Route>
-
-          </Route>
-
-        </Routes>
-
-      </ScrollToTop>
-
-    </BrowserRouter>)
-
-
-    // Reload window when changes are detected
-    let lastCheckedTimestamp = new Date().getTime()
-
-
-    setInterval(() => {
-      let now = new Date().getTime()
-      if ((now - lastCheckedTimestamp) > 60000) {
-        lastCheckedTimestamp = now
-        get("https://challenges.darkintaqt.com/api/v2/v/?t=" + now, function (e) {
-          if (e[0] !== window.versionHash) {
-            window.reloadLocation = true
-          }
-        }, function () { document.location.reload() });
-      }
-    }, config.config.reloadAfter);
-  } catch (e) {
-
-    console.warn(e)
-    root.render(<p style={{ color: "white" }}>It seems like your browser does not support this website. Please update your browser to use this website</p>)
-
   }
+
+  const ScrollToTop = ({ children }) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children
+  }
+
+  const Community = Loadable({
+    loader: (content) => import('./module/Community'),
+    loading: function () {
+      return <div style={{ width: "100%", float: "left" }}>
+        <Loader />
+        <p style={{ color: "white", fontSize: "1rem", textAlign: "center" }}>Loading...</p>
+      </div>
+    },
+  });
+
+  root.render(<BrowserRouter>
+
+    {/* SCROLL TO TOP ON ROUTE CHANGE */}
+    <ScrollToTop>
+
+      <Routes>
+
+        <Route path="/" element={<Fragment>
+          <HeaderNFooter />
+          <VariableProxy />
+        </Fragment>}>
+
+          <Route path="" element={<Start />}></Route>
+
+          <Route path="faq" element={<FAQ />}></Route>
+
+          <Route path="community" element={<Community />}></Route>
+
+          <Route path="titles" element={<Title />}></Route>
+
+          <Route path="challenges" element={<Challenges />}></Route>
+
+          <Route path="challenge/:id" element={<PathProxy />}></Route>
+
+          <Route path=":server/:user/*" element={<PathProxy />}></Route>
+
+          <Route path="*" element={<Error />}></Route>
+
+        </Route>
+
+      </Routes>
+
+    </ScrollToTop>
+
+  </BrowserRouter>)
+
+
+  // Reload window when changes are detected
+  let lastCheckedTimestamp = new Date().getTime()
+
+
+  setInterval(() => {
+    let now = new Date().getTime()
+    if ((now - lastCheckedTimestamp) > 60000) {
+      lastCheckedTimestamp = now
+      get("https://challenges.darkintaqt.com/api/v2/v/?t=" + now, function (e) {
+        if (e[0] !== window.versionHash) {
+          window.reloadLocation = true
+        }
+      }, function () { document.location.reload() });
+    }
+  }, config.config.reloadAfter);
+
 
 }
 
