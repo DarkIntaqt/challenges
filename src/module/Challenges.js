7e4652638b02cc14be1d9ed664a85e957fe22731
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-02-14 17:28:29 -0600

    Persist state of search params and filters

--- src/module/Challenges.js
+++ src/module/Challenges.js
@@ -19,6 +19,7 @@ import { setCookie } from "../func/cookiefunctions"
 import Error from "./Error"
 import { withTranslation } from "react-i18next"
 import { capitalize } from "../func/stringManipulation"
+import { storageKeys, getStorage, setStorage } from "../func/localStorageFunctions"
 
 class Challenges extends Component {
     constructor(props) {
@@ -27,9 +28,10 @@ class Challenges extends Component {
         this.toLoad = { "content": [], "leaderboards": [] };
         this.loadChallenges = this.loadChallenges.bind(this)
         this.changeFilter = this.changeFilter.bind(this)
+        this.applyFilters = this.applyFilterStyles.bind(this)
         this.showChallenges = this.showChallenges.bind(this)
-        this.filter = { "category": [], "type": [], "gamemode": [] }
-        this.searchFor = "";
+        this.filter = getStorage(storageKeys.challengeFilters, { "category": [], "type": [], "gamemode": [] })
+        this.searchFor = getStorage(storageKeys.challengeSearch, "");
 
         const LoadEvent = Loadable({
             loader: (content) => import('./events/Event.js'),
@@ -301,7 +303,22 @@ class Challenges extends Component {
 
     componentDidMount() {
         document.title = "Loading..."
+        this.applyFilterStyles();
         this.load()
+    }
+
+    applyFilterStyles() {
+      const buttons = document.querySelectorAll('button[data-id]');
+      const filters = this.filter;
+      if (filters == null) return;
+
+      const dataFilters = [...filters.category, ...filters.type, ...filters.gamemode ];
+
+      for (const button of buttons) {
+        const dataId = button.getAttribute("data-id");
+        // Apply selected CSS if data id is found in saved filters.
+        if (dataFilters.includes(dataId)) button.classList.add(filterCSS["selected"]);
+      }
     }
 
     changeFilter(e) {
@@ -324,6 +341,7 @@ class Challenges extends Component {
                     "type": this.filter.type,
                     "gamemode": this.filter.gamemode
                 }
+                setStorage(storageKeys.challengeFilters, this.filter);
 
                 break;
             case "type":
@@ -332,6 +350,7 @@ class Challenges extends Component {
                     "category": this.filter.category,
                     "gamemode": this.filter.gamemode
                 }
+                setStorage(storageKeys.challengeFilters, this.filter);
 
                 break;
             default:
@@ -340,6 +359,7 @@ class Challenges extends Component {
                     "type": this.filter.type,
                     "category": this.filter.category
                 }
+                setStorage(storageKeys.challengeFilters, this.filter);
 
                 break;
         }
@@ -347,7 +367,9 @@ class Challenges extends Component {
     }
 
     search(e) {
-        this.searchFor = e.target.value;
+        const value = e.target.value;
+        this.searchFor = value;
+        setStorage(storageKeys.challengeSearch, value)
         this.showChallenges(window.JSONPREQUEST)
     }
 
@@ -369,7 +391,7 @@ class Challenges extends Component {
                 {this.state.event}
             </section>
 
-            <input type="text" placeholder={t("Search for a challenge")} onKeyUp={this.search} className={filterCSS.input} />
+            <input type="text" placeholder={t("Search for a challenge")} onKeyUp={this.search} className={filterCSS.input} defaultValue={this.searchFor} />
             <div className={filterCSS.filter}>
 
                 <div className={filterCSS.selectors + " clearfix"}>
