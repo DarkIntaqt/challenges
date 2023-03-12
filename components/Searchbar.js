81748652dd5e9f6848111694bc53aacaf804081c
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2023-03-12 11:00:29 +0100

    You can search challenges from the startpage now

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,40 +1,169 @@
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import ChallengeService from "challenges/services/ChallengeService";
-import css from "challenges/styles/index.module.scss";
+import Image from "next/image";
+import Link from "next/link";
 import { Component, createRef } from "react";
+
+import css from "challenges/styles/index.module.scss";
+
+import ChallengeService from "challenges/services/ChallengeService";
+import ContentService from "challenges/services/ContentService";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+
+/**
+ * @typedef CardProps
+ * @type {Object}
+ * @property {string} url
+ * @property {string} title
+ * @property {string} image
+ */
+
+
+/**
+ * Returns the corresponding card for the searchBar
+ * @param {CardProps} props 
+ * @returns 
+ */
+function Card({ url, title, image }) {
+   return <Link href={url} className={css.card} prefetch={false}>
+      <div className={css.inner}>
+         <div className={css.content}>
+            <Image
+               src={image}
+               height={60}
+               width={60}
+               alt=""
+               unoptimized
+            />
+         </div>
+      </div>
+      <p>
+         {title}
+      </p>
+   </Link>;
+}
+
+
+/**
+ * fix a safari bug where the height of a backdrop-element 
+ * needs to be at least 1. 
+ * otherwise, the background is not shown
+ */
+const empty = <div style={{ height: "1px" }}></div>;
+
 
 export default class Searchbar extends Component {
    constructor() {
       super();
+      this.defaultRegion = "na1"; // change within componentDidMount to localstorage.default
 
-      this.defaultRegion = "na1";
 
-      // SELECT EFFECT
-      this.searchbarInput = createRef(null);
-      this.searchbarArea = createRef(null);
+      this.searchbarInput = createRef(null); // .searchbar
+      this.searchbarArea = createRef(null); // .searchbarWrapper
+
+      this.contentService = new ContentService(); // serving challenge and profile images
 
       this.handleFocus = this.handleFocus.bind(this);
       this.handleBlur = this.handleBlur.bind(this);
+      this.search = this.search.bind(this);
+
+      this.focus = false; // searchbar is focussed
+      this.visible = false; // component is rendered
 
       this.state = {
-         challenges: {}
+         challenges: {},
+         titles: {},
+         results: empty
       };
    }
 
+
+   /**
+    * adds the "active" class if the searchbar is focussed
+    */
    handleFocus() {
+      this.focus = true;
       this.searchbarArea.current.classList.add(css.active);
    }
 
+
+   /**
+    * removes the "active" class if the searchbar is no longer focussed
+    */
    handleBlur() {
-      this.searchbarArea.current.classList.remove(css.active);
+      this.focus = false;
+
+      setTimeout(() => {
+         if (this.focus === false && this.visible === true) {
+            this.searchbarArea.current.classList.remove(css.active);
+         }
+      }, 125);
+   }
+
+
+   /**
+    * sets the state to the current search results
+    * @param {Object} e - keyup event
+    * @returns null
+    */
+   search(e) {
+      const value = e.target.value.toLowerCase();
+
+      if (value.length === 0) {
+         /**
+          * set to default state, like last X users searched
+          */
+         return;
+      }
+
+      var challenges = this.state.challenges.map((challenge) => {
+
+         if (!challenge.name.toLowerCase().startsWith(value)) {
+            return null;
+         }
+
+         return <Card
+            key={challenge.id}
+            title={challenge.name}
+            url={"/challenges/" + challenge.id}
+            image={this.contentService.getChallengeTokenIcon(challenge.id)}
+         />;
+
+      }).filter(x => !!x);
+
+      if (challenges.length === 0) {
+         this.setState({
+            results: empty
+         });
+         return;
+      }
+
+      if (challenges.length > 6) {
+         challenges = challenges.slice(0, 6);
+      }
+
+
+      this.setState({
+         results:
+            <div className={css.category}>
+               <p>Challenges</p>
+               <div>
+                  {challenges}
+               </div>
+            </div>
+      });
    }
 
    async componentDidMount() {
 
+      this.visible = true;
+
       try {
          this.searchbarInput.current.addEventListener("focus", this.handleFocus);
          this.searchbarInput.current.addEventListener("blur", this.handleBlur);
+
+         this.searchbarInput.current.addEventListener("keyup", this.search);
       } catch (e) {
          console.warn(e);
       }
@@ -45,11 +174,17 @@ export default class Searchbar extends Component {
       // TODO: Error Handling is missing here
 
       this.setState({
-         challenges: all.challenges,
+         challenges: Object.keys(all.challenges).map((k) => all.challenges[k]),
          titles: all.titles
       });
 
    }
+
+   componentWillUnmount() {
+      this.visible = false;
+   }
+
+
    render() {
 
 
@@ -77,6 +212,11 @@ export default class Searchbar extends Component {
                icon={faMagnifyingGlass}
             />
 
+         </div>
+
+         {/* Position absolute, does not interfere the initial layout*/}
+         <div className={css.results}>
+            {this.state.results}
          </div>
 
       </div>;
