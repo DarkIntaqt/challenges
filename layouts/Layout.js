6f0ff6272c231de6e9e1f12fdf7a3967b6a23d72
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2023-03-12 11:00:48 +0100

    fix a bug where the fonts werent loaded properly

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -5,18 +5,25 @@ import Footer from "./Footer";
 export default function Layout(props) {
 
    let attributes = {};
+   let classNames = [css.wrapper];
 
    for (const attribute in props) {
       if (Object.hasOwnProperty.call(props, attribute)) {
+
+         if (attribute === "className") {
+            classNames.push(props[attribute]);
+            continue;
+         }
 
          if (attribute !== "children") {
             attributes[attribute] = props[attribute];
          }
 
+
       }
    }
 
-   return <section {...props} className={css.wrapper}>
+   return <section {...props} className={classNames.join(" ")}>
 
       <Header />
 
