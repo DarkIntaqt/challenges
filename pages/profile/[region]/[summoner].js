2eb73695ddf82f208cbfe139b339e9ced0a82867
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2023-04-04 16:41:24 +0200

    added validation for the profile page

--- pages/profile/[region]/[summoner].js
+++ pages/profile/[region]/[summoner].js
@@ -1,3 +1,23 @@
-export default function Profile() {
+import { serversRaw } from "challenges/utils/platform";
+
+export default function Profile({ user = {}, err = {} }) {
    return <span>Profile</span>;
 }
+
+
+Profile.getInitialProps = async (ctx) => {
+
+   const region = ctx.query.region;
+
+   if (!serversRaw.includes(region)) {
+      ctx.res.statusCode = 404;
+      return {
+         err: {
+            statusCode: 404,
+            message: "Invalid region"
+         }
+      };
+   }
+
+   return {};
+};
