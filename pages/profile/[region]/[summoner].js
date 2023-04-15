690008b756cdce5d36347181f002521443540d45
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2023-04-15 20:15:00 +0200

    added filters to the summoner page

--- pages/profile/[region]/[summoner].js
+++ pages/profile/[region]/[summoner].js
@@ -1,10 +1,13 @@
 import ErrorPage from "challenges/components/ErrorPage";
+import ChallengeService from "challenges/services/ChallengeService";
+import ContentService from "challenges/services/ContentService";
 import UserService from "challenges/services/UserService";
 import getPlatform, { serversBeautified } from "challenges/utils/platform";
 
-export default function Profile({ user = {}, err }) {
+export default function Profile({ user = {}, challengesRaw = {}, filters = {}, err }) {
 
    if (err) {
+      console.log(err);
       return <ErrorPage></ErrorPage>;
    }
 
@@ -31,9 +34,62 @@ Profile.getInitialProps = async (ctx) => {
       const userService = new UserService();
 
       const user = await userService.getUser(ctx.query.summoner, getPlatform(ctx.query.region));
+      const challengeService = new ChallengeService();
+      const contentService = new ContentService();
+
+      let challengesRaw = await challengeService.list(getPlatform(ctx.query.region), "en_US");
+
+      const filters = {
+         imagination: {
+            src: contentService.getChallengeTokenIcon(1),
+            name: challengesRaw[1].name
+         },
+         expertise: {
+            src: contentService.getChallengeTokenIcon(2),
+            name: challengesRaw[2].name
+         },
+         veterancy: {
+            src: contentService.getChallengeTokenIcon(3),
+            name: challengesRaw[3].name
+         },
+         teamwork: {
+            src: contentService.getChallengeTokenIcon(4),
+            name: challengesRaw[4].name
+         },
+         collection: {
+            src: contentService.getChallengeTokenIcon(5),
+            name: challengesRaw[5].name
+         },
+         legacy: {
+            src: "https://cdn.darkintaqt.com/lol/static/challenges/legacy.svg",
+         },
+         seasonal2023: {
+            src: "https://cdn.darkintaqt.com/lol/static/challenges/2023seasonal.svg",
+         },
+         seasonalRetired: {
+            src: "https://cdn.darkintaqt.com/lol/static/challenges/retired.svg",
+         },
+         summonersrift: {
+            src: "https://lolcdn.darkintaqt.com/cdn/sr.svg",
+         },
+         aram: {
+            src: "https://lolcdn.darkintaqt.com/cdn/ha.svg",
+         },
+         bot: {
+            src: "https://lolcdn.darkintaqt.com/cdn/bot.png",
+         },
+         eternals: {
+            src: "https://cdn.darkintaqt.com/lol/static/challenges/eternals.webp"
+         },
+         clash: {
+            src: "https://cdn.darkintaqt.com/lol/static/challenges/clash.webp"
+         }
+      };
 
       return {
-         user
+         user,
+         challengesRaw,
+         filters
       };
 
    } catch (e) {
@@ -41,7 +97,7 @@ Profile.getInitialProps = async (ctx) => {
       return {
          err: {
             statusCode: 404,
-            message: "Summoner not found"
+            message: e
          }
       };
 
