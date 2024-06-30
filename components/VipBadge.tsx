341d1a12db089e2394444cd138e599b920f296f2
Michael M <8100939+GinMitch@users.noreply.github.com>
2024-06-30 14:40:08 +0200

    fix: vip badge size and margin

--- components/VipBadge.tsx
+++ components/VipBadge.tsx
@@ -1,30 +1,26 @@
-import VerifiedIcon from "challenges/assets/verified.svg";
 import { Property } from "csstype";
 import { ReactNode } from "react";
 
-export default function VipBadge({
-   size = "20px",
-   position = "relative",
-   margin = "0 5px",
-   verified = false,
-}: VipBadgeProps): ReactNode {
+import VerifiedIcon from "challenges/assets/verified.svg";
+
+export default function VipBadge({ size = "20px", position = "relative", verified = false }: VipBadgeProps): ReactNode {
    return (
-      <VerifiedIcon style={{
-         height: `calc(0.7 * ${size})`,
-         width: `calc(0.7 * ${size})`,
-         borderRadius: "50%",
-         margin: margin,
-         top: "0",
-         left: "0",
-         filter: verified ? "" : "grayscale(100%)",
-         position: position,
-      }} />
+      <VerifiedIcon
+         style={{
+            height: `calc(0.7 * ${size})`,
+            width: `calc(0.7 * ${size})`,
+            borderRadius: "50%",
+            top: "0",
+            left: "0",
+            filter: verified ? "" : "grayscale(100%)",
+            position: position,
+         }}
+      />
    );
 }
 
 interface VipBadgeProps {
    size?: string;
    position?: Property.Position;
-   margin?: string;
    verified?: boolean;
 }
