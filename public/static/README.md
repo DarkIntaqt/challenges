347253783371c59e03831f9afba0199041f5215a
DarkIntaqt <git@darkintaqt.dev>
2025-10-01 21:16:06 +0200

    feat: OS communities

---
+++ public/static/README.md
@@ -0,0 +1,7 @@
+## Communities
+
+Please add communities to the bottom, I might re-arrange these later.
+
+### Images
+
+Please upload an image for your community in the pullrequest. Set the `image`-field in the JSON to `https://lolcdn.darkintaqt.com/s/<TERM>.<FILETYPE>`. The `TERM` is a URL-encoded word/id that uniquely identifies your community. Supported file types are .png, .webp and .jpg.
--- src/module/Community.js
+++ src/module/Community.js
@@ -6,106 +6,134 @@ import get from "../func/get";
 
 import Loader from "./Loader";
 
-import css from "../css/community.module.css"
+import css from "../css/community.module.css";
 
-const endpoint = "https://challenges.darkintaqt.com/community.json";
+const endpoint = "/static/community.json";
 const defaultLang = "en";
 
 const langs = navigator.languages || ["en"];
 
 export default class Community extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        let communities = [];
+    let communities = [];
 
-        const request = getCache(endpoint)
+    const request = getCache(endpoint);
 
-        if (request !== false) {
-            communities = request
-        }
-
-        this.state = {
-            communities
-        }
-
-        this.loadedStats = this.loadedStats.bind(this)
+    if (request !== false) {
+      communities = request;
     }
 
+    this.state = {
+      communities,
+    };
 
+    this.loadedStats = this.loadedStats.bind(this);
+  }
 
-    loadedStats(data) {
-        this.setState({ communities: data })
+  loadedStats(data) {
+    this.setState({ communities: data });
+  }
+
+  componentDidMount() {
+    if (this.state.communities.length === 0) {
+      get(endpoint, this.loadedStats);
+    }
+  }
+
+  render() {
+    if (this.state.communities.length === 0) {
+      return (
+        <div style={{ width: "100%", float: "left" }}>
+          <Loader />
+          <p style={{ color: "white", fontSize: "1rem", textAlign: "center" }}>
+            Loading Communities...
+          </p>
+        </div>
+      );
     }
 
+    document.title = "League of Legends Challenge Community";
 
-    componentDidMount() {
+    return (
+      <div className={css.bgArea}>
+        <section className={`object1000`}>
+          <div className={css.topArea}>
+            <h1 className={css.heading}>
+              Meet the <br />
+              Challenge-Community
+            </h1>
+          </div>
+        </section>
 
-        if (this.state.communities.length === 0) {
-            get(endpoint, this.loadedStats)
-        }
-
-    }
-
-
-    render() {
-
-        if (this.state.communities.length === 0) {
-            return <div style={{ width: "100%", float: "left" }}>
-                <Loader />
-                <p style={{ color: "white", fontSize: "1rem", textAlign: "center" }}>Loading Communities...</p>
-            </div>
-        }
-
-        document.title = "League of Legends Challenge Community"
-
-        return <div className={css.bgArea}>
-            <section className={`object1000`}>
-
-                <div className={css.topArea}>
-                    <h1 className={css.heading}>Meet the <br />Challenge-Community</h1>
-                </div>
-
-
+        <div className={css.scrollSection}>
+          <section className={`object1000`} style={{ position: "relative" }}>
+            <section
+              className={`${css.community} ${css.disclaimer} GRANDMASTER`}
+            >
+              <div>
+                <p data-nosnippet>
+                  External links and Discord servers are provided for
+                  convenience. No affiliation, endorsement or compensation is
+                  involved. External content is not actively monitored, and
+                  responsibility for external sites lies with their operators.
+                  See the <a href="/tos">TOS</a> for further information.
+                  <br />
+                  Do you know a cool tool that is missing? Add your community{" "}
+                  <a
+                    href="https://github.com/DarkIntaqt/challenges/tree/master/public/static"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    here
+                  </a>
+                  .
+                </p>
+              </div>
             </section>
 
-            <div className={css.scrollSection}>
-                <section className={`object1000`} style={{ position: 'relative' }}>
+            {this.state.communities.map((community, i) => {
+              let uselang = defaultLang;
+              for (let i = 0; i < langs.length; i++) {
+                if (community.langs.includes(langs[i])) {
+                  uselang = langs[i];
+                  break;
+                }
+              }
 
-                    {this.state.communities.map((community, i) => {
+              // in case a community isnt available in default lang and other langs are not available
+              if (!community[uselang]) {
+                return <></>;
+              }
 
-                        let uselang = defaultLang;
-                        for (let i = 0; i < langs.length; i++) {
-                            if (community.langs.includes(langs[i])) {
-                                uselang = langs[i];
-                                break;
-                            }
-                        }
+              const translated = community[uselang];
 
-                        // in case a community isnt available in default lang and other langs are not available
-                        if (!community[uselang]) {
-                            return <></>;
-                        }
-
-                        const translated = community[uselang];
-
-                        return <div className={css.community} key={i}>
-                            <div>
-                                <h2>{translated.title}</h2>
-                                <p>{translated.description}
-                                    <span>
-                                        <a href={translated.link}><i className={community.icon} /> {translated.cta}</a>
-                                    </span>
-                                </p>
-                            </div>
-                            <LazyLoadImage alt={translated.title} src={community.image} width={210} height={210} />
-                        </div>;
-                    })}
-
-                </section>
-
-            </div>
-
+              return (
+                <div className={css.community} key={i}>
+                  <div>
+                    <h2>{translated.title}</h2>
+                    <p>
+                      {translated.description}
+                      <span>
+                        <a href={translated.link}>
+                          <i className={community.icon} /> {translated.cta}
+                        </a>
+                      </span>
+                    </p>
+                  </div>
+                  <LazyLoadImage
+                    alt={translated.title}
+                    src={community.image}
+                    width={210}
+                    height={210}
+                  />
+                </div>
+              );
+            })}
+          </section>
         </div>
-    }
+      </div>
+    );
+  }
 }
