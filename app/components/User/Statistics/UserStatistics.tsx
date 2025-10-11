d0064e572df6c99213d79bf960e2a9f97b9c9009
DarkIntaqt <git@darkintaqt.dev>
2025-10-11 10:47:06 +0200

    feat: bump distribution

--- app/components/User/Statistics/UserStatistics.tsx
+++ app/components/User/Statistics/UserStatistics.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "@mui/material/styles";
+import { LineChart } from "@mui/x-charts";
+import { axisClasses } from "@mui/x-charts/ChartsAxis";
 import Heading from "@cgg/components/Heading/Heading";
-import { useStaticData } from "@cgg/hooks/useStaticData";
+import { capitalize } from "@cgg/utils/capitalize";
 import type { IApiChallengeResponse } from "@cgg/utils/endpoints/types";
+import { tierList } from "@cgg/utils/suffixToTier";
 import Categories from "./Categories";
+import Distribution from "./Distribution";
+import darkTheme from "./theme";
 import css from "./userStatistics.module.scss";
 
 export default function UserStatistics({
@@ -10,11 +16,17 @@ export default function UserStatistics({
    playerData: IApiChallengeResponse;
 }) {
    return (
-      <div className={css.wrapper}>
-         <div className={css.section}>
-            <Heading level={2}>Categories</Heading>
-            <Categories playerData={playerData} />
+      <ThemeProvider theme={darkTheme}>
+         <div className={css.wrapper}>
+            <div className={css.section} style={{ gridArea: "1 / 1 / 2 / 3" }}>
+               <Heading level={2}>Categories</Heading>
+               <Categories playerData={playerData} />
+            </div>
+            <div className={css.section} style={{ gridArea: "2 / 2 / 3 / 3" }}>
+               <Heading level={2}>Distribution</Heading>
+               <Distribution playerData={playerData} />
+            </div>
          </div>
-      </div>
+      </ThemeProvider>
    );
 }
